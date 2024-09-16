c59ea0f96d2b69e0586520efc30afb7c729f9253
Anaf Talukder <111679954+Anaf-Talukder@users.noreply.github.com>
2024-09-16 21:09:53 +0600

    yes

--- scripts/main.js
+++ scripts/main.js
@@ -19,3 +19,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+let bg_main = document.getElementById('bg_main')
+        let buildings = document.getElementById('buildings')
+        let bg_front_a = document.getElementById('bg_front_a')
+
+        window.addEventListener('scroll', function(){
+            let value = window.scrollY;
+            buildings.style.top = -value * 0.3 + "px";
+            bg_front_a.style.top = -value * 0.7 + "px";
+        })
+
